71f93ac60d9c0800af2efdb5c6e66944bb43203d
egrishina <egrishina22@fed.idserve.net>
2022-12-06 16:37:23 +0100

    Initial Commit

---
+++ Freesbe/FreesbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreesbeApp.swift
+//  Freesbe
+//
+//  Created by Ekaterina Grishina on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FreesbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
